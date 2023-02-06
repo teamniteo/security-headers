0ae94d15bcdb23b4084019145ff46e8633f77366
Janez Troha <239513+dz0ny@users.noreply.github.com>
2023-02-06 10:42:35 +0000

    Update GH state to new format

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,25 +22,28 @@ declare -A grades=(
 )
 
 function scan() {
-    curl -slvo /dev/null --show-error --fail "$1" --tlsv$2 --tls-max $2
+    curl -slo /dev/null --show-error --fail "$1" --tlsv$2 --tls-max $2
     retval=$?
     if [ $retval -eq 0 ]; then
         supportedTLS=$(echo "$supportedTLS tls$2" | xargs)
+		echo "supportedTLS=$supportedTLS"
     fi
 }
 
-RATING=$(curl -s -L "https://securityheaders.com/?hide=on&followRedirects=on&q=https%3A%2F%2F$1" -I | sed -En 's/x-grade: (.*)/\1/p' | tr -d '\r')
+RATING=$(curl -s -L "https://securityheaders.com/?hide=on&followRedirects=on&q=$1" -I | sed -En 's/x-grade: (.*)/\1/p' | tr -d '\r')
+
 scan "$1" "1.0"
 scan "$1" "1.1"
 scan "$1" "1.2"
 scan "$1" "1.3"
 
-if [ "$supportedTLS" != "tls1.2 tls1.3" ]; then
+# Protocol list does not include tls1.2 or tls1.3
+if [[ ! "$supportedTLS" =~ "tls1.2 tls1.3" ]]; then
 	RATING="F"
 fi
 
-echo "::set-output name=supportedTLS::$supportedTLS"
-echo "::set-output name=rating::$RATING"
+echo "{supportedTLS}={$supportedTLS}" >> $GITHUB_STATE
+echo "{rating}={$RATING}" >> $GITHUB_STATE
 
 if [ ${grades[$RATING]} -ge ${grades[$GRADE]} ]; then
 	exit 0
